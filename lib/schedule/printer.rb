849d172c9c7ad7321aa4f9d58c31e9a8f355d5f6
Benjamin Smith <benjamin.lee.smith@gmail.com>
2012-09-23 12:24:56 -0600

    worked on alignment

--- lib/schedule/printer.rb
+++ lib/schedule/printer.rb
@@ -18,6 +18,9 @@ module Schedule
 
     def print_schedule_table(data)
       table = Terminal::Table.new :headings => ["Time", "Track 1", "Track 2"], :rows => data
+      table.align_column(0, :center)
+      table.align_column(1, :center)
+      #table.align_column(2, :center)
 
       puts table.render
     end
